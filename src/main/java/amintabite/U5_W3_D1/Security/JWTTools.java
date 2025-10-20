308df7dd0c281e6e46db1016592244e6477f543a
AminTabite <Aminepicode13@gmail.com>
2025-10-20 16:46:48 +0200

    file Jwttools fatto

--- src/main/java/amintabite/U5_W3_D1/Security/JWTTools.java
+++ src/main/java/amintabite/U5_W3_D1/Security/JWTTools.java
@@ -1,12 +1,27 @@
 package amintabite.U5_W3_D1.Security;
 
+import amintabite.U5_W3_D1.Entities.Dipendente;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.util.Date;
 
 @Component
 public class JWTTools {
 
     @Value("${jwt.secret}")
     private String secret;
+
+    public String createToken(Dipendente dipendente) {
+
+        return Jwts.builder()
+                .issuedAt(new Date(System.currentTimeMillis()))
+                .expiration(new Date(System.currentTimeMillis() + 1000*60*60*24))
+                .subject(String.valueOf(dipendente.getIddipendente()))
+                .signWith(Keys.hmacShaKeyFor(secret.getBytes()))
+                .compact();
+    }
 
 }
